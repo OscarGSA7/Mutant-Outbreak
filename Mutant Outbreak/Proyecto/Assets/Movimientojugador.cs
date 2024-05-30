41e8321504984739da7c142a45d258317dfc949e
Mrtacos91 <tacosricos_18@outlook.com>
2024-05-29 22:42:14 -0600

    MutantOutbreak(Work in progess)
    - Se elimino la escena de ejemplo
    - Se hizo la función de jugar cambiara de escena la de el juego

--- Mutant Outbreak/Proyecto/Assets/Movimientojugador.cs
+++ Mutant Outbreak/Proyecto/Assets/Movimientojugador.cs
@@ -7,13 +7,27 @@ public class Movimiento : MonoBehaviour
     [SerializeField] private float velocidadMovimiento; 
     [SerializeField] private Vector2 direccion;
     private Rigidbody2D rb2d;
+    private float movimientoX;
+    private float movimientoY;
+    private Animator animator;
 
     private void Start() {
+        animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>(); 
     }
 
     private void Update() {
-        direccion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        movimientoX = Input.GetAxisRaw("Horizontal");
+        movimientoY = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("MovimientoX",movimientoX);
+        animator.SetFloat("MovimientoY",movimientoY);
+        if(movimientoX != 0 || movimientoY != 0){
+            animator.SetFloat("UltimoX",movimientoX);
+            animator.SetFloat("UltimoY",movimientoY);
+
+        }
+        direccion = new Vector2(movimientoX, movimientoY).normalized;
     }
 
     private void FixedUpdate() {
