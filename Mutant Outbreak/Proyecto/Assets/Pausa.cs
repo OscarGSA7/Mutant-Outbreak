e34af62cfe75691a200391db8473b370621f0222
Mrtacos91 <tacosricos_18@outlook.com>
2024-07-12 00:59:23 -0600

    MutantOutbreak(Alpha)
    - Se corrigen errores en codigo en el menu principal
    - Se añade el primer ejecutable de la version alpha

--- Mutant Outbreak/Proyecto/Assets/Pausa.cs
+++ Mutant Outbreak/Proyecto/Assets/Pausa.cs
@@ -7,23 +7,52 @@ public class Pausa : MonoBehaviour
 {
     [SerializeField] private GameObject botonPausa;
     [SerializeField] private GameObject menuPausa;
-    public void pausa(){
-        Time.timeScale = 0f;
-        botonPausa.SetActive(false);
-        menuPausa.SetActive(true);
+
+    private bool isPaused = false;
+
+    public void pausa()
+    {
+        if (!isPaused)
+        {
+            Time.timeScale = 0f;
+            botonPausa.SetActive(false);
+            menuPausa.SetActive(true);
+            isPaused = true;
+        }
     }
-    
-    public void Resume(){
+
+    public void Resume()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1f;
+            botonPausa.SetActive(true);
+            menuPausa.SetActive(false);
+            isPaused = false;
+        }
+    }
+
+    public void Restart()
+    {
         Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("menuinicial"); // Cambia "MainMenu" por el nombre de la escena de inicio
+    }
+
+    private void OnEnable()
+    {
         botonPausa.SetActive(true);
         menuPausa.SetActive(false);
+        isPaused = false;
     }
-    
-    public void Restart(){
+
+    private void OnDisable()
+    {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-    public void Quit(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -2);
     }
 }
