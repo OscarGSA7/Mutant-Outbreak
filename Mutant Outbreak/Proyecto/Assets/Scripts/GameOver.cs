db862d2e9c84ef303384dacc35dcb3a39124cc61
JosueZurita <josuezurita.golli@gmail.com>
2024-07-29 18:58:28 -0600

    Mutan Outbreak
    Se agrego el botón en el Game Over de regresar al menú

--- Mutant Outbreak/Proyecto/Assets/Scripts/GameOver.cs
+++ Mutant Outbreak/Proyecto/Assets/Scripts/GameOver.cs
@@ -10,6 +10,11 @@ public class GameOver : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
+    public void Regresar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+
         
     
 }
